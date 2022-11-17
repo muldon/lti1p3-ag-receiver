cf2b610ff4eead23a391b77689f337f683a8cd7c
Rodrigo Silva <rsilva@carnegielearning.com>
2022-11-17 08:37:33 -0300

    renaming project

--- src/test/java/cl/services/lti1p3/ag/receiver/ReceiverApplicationTests.java
+++ src/test/java/cl/services/lti1p3/ag/receiver/ReceiverApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EvolutionApplicationTests {
+class ReceiverApplicationTests {
 
 	@Test
 	void contextLoads() {
